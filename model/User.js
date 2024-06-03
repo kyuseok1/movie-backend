b5c01b90fccc1fbac5c563919e4bddd684f3b1c3
kyuseok1 <jks980308@naver.com>
2024-06-04 03:32:55 +0900

    movie-server

--- model/User.js
+++ model/User.js
@@ -1,9 +1,11 @@
-// backend/models/User.js
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -12,32 +12,42 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://llv7417:whrbtjr123@cluster0.wewyamo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose
+  .connect(
+    "mongodb+srv://llv7417:whrbtjr123@cluster0.wewyamo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // User schema and model
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
 });
 
 const User = mongoose.model("User", UserSchema);
 
-// Registration route
 app.post("/Register", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, email } = req.body;
+  console.log("Received registration request: ", req.body);
+  console.log("Email received: ", email); // 디버깅용 로그
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const user = new User({ username, password: hashedPassword });
+    const user = new User({ username, password: hashedPassword, email });
     await user.save();
     res.status(201).send("User registered");
   } catch (error) {
+    console.error("Error registering user: ", error);
     res.status(500).send("Error registering user");
   }
 });
