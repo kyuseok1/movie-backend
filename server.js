050ef956f2c99ea5901eb377c8a2dda43e0cb4f5
kyuseok1 <jks980308@naver.com>
2024-06-03 19:54:27 +0900

    Your commit message

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post("/Login", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
