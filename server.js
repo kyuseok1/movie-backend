33fb24704db4f4504f6f3b4ae3bfd2d9b9a26659
kyuseok1 <jks980308@naver.com>
2024-06-10 06:47:44 +0900

    api url

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${process.env.CLIENT_ORIGIN}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const { id, displayName, emails } = profile;
